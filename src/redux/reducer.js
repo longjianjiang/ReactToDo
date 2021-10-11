068524772872067bbdb6ee41203e656ccb2a9af1
longjianjiang <616393956@qq.com>
2021-10-11 16:25:11 +0800

    Update

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import todoList from "./todoList/reducer";
 import todoFilter from "./todoFilter/reducer";
 
+// export default : https://www.jianshu.com/p/edaf43e9384f
 export default combineReducers({
     todoList,
     todoFilter,
